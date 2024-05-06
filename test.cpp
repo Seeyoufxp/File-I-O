aff220ecdba1f086b66ed38dbaa25126c5217505
Seeyoufxp <seeyoufxp@gmail.com>
2024-05-06 17:01:00 +0800

    2

--- test.cpp
+++ test.cpp
@@ -5,13 +5,13 @@
 void test01()
 {
     std::ofstream ofs;
-    ofs.open("text.txt",std::ios::out);
+    ofs.open("test01.txt",std::ios::out);
     ofs << "man" << std::endl;
     ofs << "laoaba" << std::endl;
     ofs.close();
 
     std::ifstream ifs;
-    ifs.open("text.txt",std::ios::in);
+    ifs.open("test01.txt",std::ios::in);
     if(!ifs.is_open())
     {
         std::cout << "Error" << std::endl;
@@ -46,9 +46,36 @@ void test01()
     ofs.close();
 }
 
+class Person
+{
+public:
+    char m_Name[64];
+    int m_Age;
+};
+
+void test02()
+{
+    std::ofstream ofs;
+    ofs.open("test02.txt", std::ios::out | std::ios::binary);
+    Person p = {"laoba", 18};
+    ofs.write((const char *)&p, sizeof(Person));
+    ofs.close();
+
+    std::ifstream ifs;
+    ifs.open("test02.txt", std::ios::in | std::ios::binary);
+    if(!ifs.is_open())
+    {
+        std::cout << "Error" << std::endl;
+    }
+    ifs.read((char *)&p, sizeof(Person));
+    std::cout << p.m_Name << " " << p.m_Age << " " << std::endl;
+    ifs.close();
+}  
+
 int main()
 {
     test01();
+    test02();
 
     system("pause");
     return 0;
